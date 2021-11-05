a8d0097a4bbc0e6cf510a008964c93c057d74c22
Camilo <benitocamelo0002@gmail.com>
2021-11-05 17:23:55 -0500

    Subiendo cambios

---
+++ js/bootrstrap-elementos.js
@@ -0,0 +1,25 @@
+/* Activar Popover */
+window.addEventListener("load", function(){
+    /* Tooltip */
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    /* Popover (Requiere Tooltip) */
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+  
+})
+});
+/* Toast */
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+
